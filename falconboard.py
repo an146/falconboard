fa0cbdc429c4a85b6232630150413511d0312340
Anton Novikov <random.plant@gmail.com>
2017-07-25 01:00:54 +0300

    simple json board functionality

--- falconboard.py
+++ falconboard.py
@@ -1,27 +1,55 @@
 import json
 import logging
 import uuid
+import traceback
 from wsgiref import simple_server
 
 import falcon
 import pymongo
 
 
+logging.basicConfig(level=logging.DEBUG)
+
+def mongo_limit(cursor, limit):
+    return cursor.skip(cursor.count() - limit)
+
 class StorageEngine:
     client = pymongo.MongoClient('mongodb://falconboard:password@ds161022.mlab.com:61022/falconboard')
     db = client.get_default_database()
 
-    def get_threads(self, board_id):
-        if not board_id in ['b']:
+    def check_board(self, board):
+        if not board in ['b']:
             raise falcon.HTTPError(falcon.HTTP_404, 'Unknown board', 'Board not found')
-        coll = self.db['board.' + board_id]
-        threads = []
-        for thread in coll.find({"is_thread": True}):
-            comments = coll.find({"parent_id": thread["_id"]}).skip(coll.count() - 3)
-            threads = threads + [thread] + list(comments)
-        return threads
 
-    pass
+    def check_post(self, post):
+        for field in post:
+            if not field in ['title', 'text', 'parent_id', 'sage']:
+                raise falcon.HTTPError(falcon.HTTP_403, 'Invalid argument', 'Unknown field: ' + field)
+
+    def get_posts(self, board):
+        self.check_board(board)
+        coll = self.db['board.' + board]
+        posts = []
+        for thread in coll.find({"is_thread": True}):
+            comments = mongo_limit(coll.find({"parent_id": thread["_id"]}), 3)
+            posts = posts + [thread] + list(comments)
+        return posts
+
+    def get_post(self, board, post):
+        self.check_board(board)
+        coll = self.db['board.' + board]
+        post = coll.find_one({"_id": post})
+        comments = coll.find({"parent_id": post})
+        return [post] + list(comments)
+
+    def add_post(self, board, post):
+        self.check_board(board)
+        self.check_post(post)
+        coll = self.db['board.' + board]
+        _id = int(6)
+        post["_id"] = _id
+        return coll.insert_one(post)
+
 
 
 class StorageError(Exception):
@@ -33,10 +61,6 @@ class StorageError(Exception):
         raise falcon.HTTPError(falcon.HTTP_725,
                                'Database Error',
                                description)
-
-
-def token_is_valid(token, board_id):
-    return True  # Suuuuuure it's valid...
 
 
 def auth(req, resp, params):
@@ -55,20 +79,16 @@ def check_media_type(req, resp, params):
             href='http://docs.examples.com/api/json')
 
 
-class ThreadsResource:
-
+class PostResource:
     def __init__(self, db):
         self.db = db
-        self.logger = logging.getLogger('threadsapp.' + __name__)
 
-    def on_get(self, req, resp, board_id):
-        marker = req.get_param('marker') or ''
-        limit = req.get_param_as_int('limit') or 50
-
+    def on_get(self, req, resp, board, post):
         try:
-            result = self.db.get_threads(board_id)
+            result = self.db.get_post(board, int(post))
         except Exception as ex:
-            self.logger.error(ex)
+            traceback.print_exc()
+            logging.error(ex)
 
             description = ('Aliens have attacked our base! We will '
                            'be back as soon as we fight them off. '
@@ -79,11 +99,35 @@ class ThreadsResource:
                 description,
                 30)
 
-        resp.set_header('X-Powered-By', 'Donuts')
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(result)
 
-    def on_post(self, req, resp, board_id):
+class ThreadsResource:
+    def __init__(self, db):
+        self.db = db
+
+    def on_get(self, req, resp, board):
+        marker = req.get_param('marker') or ''
+        limit = req.get_param_as_int('limit') or 50
+
+        try:
+            result = self.db.get_posts(board)
+        except Exception as ex:
+            logging.error(ex)
+
+            description = ('Aliens have attacked our base! We will '
+                           'be back as soon as we fight them off. '
+                           'We appreciate your patience.')
+
+            raise falcon.HTTPServiceUnavailable(
+                'Service Outage',
+                description,
+                30)
+
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(result)
+
+    def on_post(self, req, resp, board):
         try:
             raw_json = req.stream.read()
         except Exception:
@@ -93,24 +137,25 @@ class ThreadsResource:
                                    'them ponies again.')
 
         try:
-            thread = json.loads(raw_json, 'utf-8')
+            post = json.loads(raw_json, 'utf-8')
         except ValueError:
             raise falcon.HTTPError(falcon.HTTP_753,
                                    'Malformed JSON',
                                    'Could not decode the request body. The '
                                    'JSON was incorrect.')
 
-        proper_thread = self.db.add_thread(thread)
+        post = self.db.add_post(board, post)
 
         resp.status = falcon.HTTP_201
-        resp.location = '/%s/threads/%s' % (board_id, proper_thread.id)
+        resp.location = '/%s/%s/' % (board, post.inserted_id)
 
 # Configure your WSGI server to load "threads.app" (app is a WSGI callable)
 app = falcon.API(before=[auth, check_media_type])
 
 db = StorageEngine()
-threads = ThreadsResource(db)
-app.add_route('/{board_id}/threads', threads)
+
+app.add_route('/{board}/', ThreadsResource(db))
+app.add_route('/{board/{post}/', PostResource(db))
 
 # If a responder ever raised an instance of StorageError, pass control to
 # the given handler.
