0df4f346df13ee026237eb53939ef3f5bcb52366
Anton Novikov <random.plant@gmail.com>
2017-07-25 19:35:03 +0300

    auth

--- falconboard.py
+++ falconboard.py
@@ -5,13 +5,19 @@ import traceback
 from wsgiref import simple_server
 
 import falcon
+import falcon_auth
 import pymongo
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 def mongo_limit(cursor, limit):
-    return cursor.skip(cursor.count() - limit)
+    skip = cursor.count() - limit
+    return cursor.skip(skip >= 0 and skip or 0)
+
+def mongo_page(cursor, page):
+    step = 3
+    return mongo_limit(cursor.sort("_id", pymongo.ASCENDING), step * (page + 1)).limit(step)
 
 class StorageEngine:
     client = pymongo.MongoClient('mongodb://falconboard:password@ds161022.mlab.com:61022/falconboard')
@@ -23,15 +29,16 @@ class StorageEngine:
 
     def check_post(self, post):
         for field in post:
-            if not field in ['title', 'text', 'parent_id', 'sage']:
+            if not field in ['title', 'text', 'parent', 'sage']:
                 raise falcon.HTTPError(falcon.HTTP_403, 'Invalid argument', 'Unknown field: ' + field)
 
     def get_posts(self, board):
         self.check_board(board)
         coll = self.db['board.' + board]
         posts = []
-        for thread in coll.find({"is_thread": True}):
-            comments = mongo_limit(coll.find({"parent_id": thread["_id"]}), 3)
+        threads = mongo_page(coll.find({"parent": None}), 0)
+        for thread in threads:
+            comments = mongo_limit(coll.find({"parent": thread["_id"]}), 3)
             posts = posts + [thread] + list(comments)
         return posts
 
@@ -39,17 +46,19 @@ class StorageEngine:
         self.check_board(board)
         coll = self.db['board.' + board]
         post = coll.find_one({"_id": post})
-        comments = coll.find({"parent_id": post})
+        comments = coll.find({"parent": post})
         return [post] + list(comments)
 
-    def add_post(self, board, post):
+    def add_post(self, board, parent, post):
         self.check_board(board)
         self.check_post(post)
         counters = self.db['counters']
-        _id = counters.find_and_modify(query={"board": board}, update={"$inc": "max_id"})
-        logging.info(_id)
+        _id = counters.find_and_modify(query={"_id": board}, update={"$inc": {"next": 1}})["next"]
+        if _id == None:
+            raise falcon.HTTPError(falcon.HTTP_500, 'Counters lost', 'Internal error, can\'t find counter for ' + board)
         coll = self.db['board.' + board]
         post["_id"] = _id
+        post["parent"] = parent
         return coll.insert_one(post)
 
 
@@ -63,15 +72,6 @@ class StorageError(Exception):
         raise falcon.HTTPError(falcon.HTTP_725,
                                'Database Error',
                                description)
-
-
-def auth(req, resp, params):
-    # Alternatively, do this in middleware
-    token = req.get_header('X-Auth-Token')
-
-    if token is None:
-        token = uuid.uuid4()
-        req.headers['X-Auth-Token'] = token
 
 
 def check_media_type(req, resp, params):
@@ -104,6 +104,31 @@ class PostResource:
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(result)
 
+    def on_post(self, req, resp, board, post):
+        post = int(post)
+        try:
+            raw_json = req.stream.read()
+        except Exception:
+            raise falcon.HTTPError(falcon.HTTP_748,
+                                   'Read Error',
+                                   'Could not read the request body. Must be '
+                                   'them ponies again.')
+
+        try:
+            comment = json.loads(raw_json, 'utf-8')
+            if not "parent" in comment:
+                comment["parent"] = post
+        except ValueError:
+            raise falcon.HTTPError(falcon.HTTP_753,
+                                   'Malformed JSON',
+                                   'Could not decode the request body. The '
+                                   'JSON was incorrect.')
+
+        comment = self.db.add_post(board, post, comment)
+
+        resp.status = falcon.HTTP_201
+        resp.location = '/%s/%s/' % (board, post)
+
 class ThreadsResource:
     def __init__(self, db):
         self.db = db
@@ -115,6 +140,7 @@ class ThreadsResource:
         try:
             result = self.db.get_posts(board)
         except Exception as ex:
+            traceback.print_exc()
             logging.error(ex)
 
             description = ('Aliens have attacked our base! We will '
@@ -146,18 +172,36 @@ class ThreadsResource:
                                    'Could not decode the request body. The '
                                    'JSON was incorrect.')
 
-        post = self.db.add_post(board, post)
+        post = self.db.add_post(board, None, post)
 
         resp.status = falcon.HTTP_201
         resp.location = '/%s/%s/' % (board, post.inserted_id)
 
+class DefaultSink(object):
+    def on_get(self, req, resp):
+        resp.body = str(req.path)
+
+def user_loader(username, password):
+    if password != username + '123':
+        raise falcon.HTTPError(falcon.HTTP_403, 'Auth failed', 'Incorrect password')
+    return {'username': username}
+auth_backend = falcon_auth.BasicAuthBackend(user_loader)
+auth_middleware = falcon_auth.FalconAuthMiddleware(auth_backend, exempt_routes=['/_api/'], exempt_methods=['HEAD', 'GET', 'POST'])
+
 # Configure your WSGI server to load "threads.app" (app is a WSGI callable)
-app = falcon.API(before=[auth, check_media_type])
+app = falcon.API(middleware=auth_middleware)
 
 db = StorageEngine()
 
-app.add_route('/{board}/', ThreadsResource(db))
-app.add_route('/{board/{post}/', PostResource(db))
+threads = ThreadsResource(db)
+post = PostResource(db)
+app.add_route('/_api/{board}', threads)
+app.add_route('/_api/{board}/', threads)
+app.add_route('/_api/{board}/{post}', post)
+app.add_route('/_api/{board}/{post}/', post)
+app.add_sink(DefaultSink().on_get, prefix='/')
+#app.add_route('/{board}/', BoardResource(db))
+#app.add_route('/{board}/{thread}', BoardResource(db))
 
 # If a responder ever raised an instance of StorageError, pass control to
 # the given handler.
